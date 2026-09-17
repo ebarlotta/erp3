85b80b592f526755bee92cb24669943e2195e6c2
Enzo Barlotta <ebarlotta@yahoo.com.ar>
2026-09-17 06:49:46 -0300

    Actualizando PWA

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => 'Ecosystems.ar',
     'manifest' => [
-        'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'name' => env('APP_NAME', 'Ecosystems App'),
+        'short_name' => 'Eocsystems',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
